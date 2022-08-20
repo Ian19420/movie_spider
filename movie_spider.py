edb930eaa707be45db0bdea590cc50ccbcc0f9b5
Ian <glps1004@gmail.com>
2022-08-20 14:25:49 +0800

    improve

--- movie_spider.py
+++ movie_spider.py
@@ -9,22 +9,22 @@ moviehtml = requests.get(url)
 soup = bs4.BeautifulSoup(moviehtml.text, "lxml")
 
 num = 0
-list_ = soup.find("ul", class_ = "release_list")
-items = list_.find_all("li")
+items = soup.find_all('div', 'release_info')
 for item in items:
     cname = item.find('div', class_ = 'release_movie_name').a.text.strip()
     ename = item.find('div', class_ = 'en').a.text.strip()
+
     r_time = item.find("div", class_ = "release_movie_time").text
     pattern = r"\d{4}-\d\d-\d\d"
     time = re.findall(pattern, r_time)
+
     level = item.find('div', 'leveltext').span.text.strip()
     txt = item.find('div', 'release_text').text.strip()
 
-    img_tap = item.find("img", class_ = "lazy-load")
-    photo_url = img_tap["data-src"]
-    picture = requests.get(photo_url)
+    photo_url = item.find_previous_sibling('div', class_ = 'release_foto').a.img["data-src"]
+    photo = requests.get(photo_url)
     with open(os.path.join(destDir, cname) + ".jpg", "wb") as f:
-        for diskStorage in picture.iter_content(10240):
+        for diskStorage in photo.iter_content(10240):
             f.write(diskStorage)
 
     num += 1
@@ -34,7 +34,6 @@ for item in items:
     print("圖片網址 ： ", photo_url)
     print('期待度 : ', level)
     print("內容摘要 ： ", txt)
-
     print("上映日期 ： ", time[0])
     print()
 
